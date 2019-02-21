59c2f1305be08c0a4b56e2528dde9bac3a50e613
xxxx591 <250173319@qq.com>
2019-02-21 10:15:12 +0800

    123

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// var HOST = 'http://192.168.1.254:5000';
+var HOST = 'http://116.62.151.17:88';
+var CLASS_DATA={
+  'ahk3':'top-cat_1301',
+  'jsk3':'top-cat_1302',
+  'gxk3':'top-cat_1303',
+  'hubk3':'top-cat_1304',
+  'nmgk3':'top-cat_1305',
+  'bjk3':'top-cat_1306',
+  'hebk3' :'top-cat_1307',
+  'shk3':'top-cat_1308',
+  'gsk3':'top-cat_1309'
+}
